0297f7c231ed074e7d4dd1e1b5eb710391e3fd2e
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-02-15 11:32:03 -0500

    Update Snake.ts

--- sandbox (5)/src/Snake.ts
+++ sandbox (5)/src/Snake.ts
@@ -5,40 +5,40 @@ import Point from "./Point";
 // place your code on line 5 above the export statement below
 
 class Snake {
-  private currentPosition: number;
-  private currentDirection: number;
-  public position: Point;
-  public direction: string;
+  //private currentPosition: number;
+  //private currentDirection: number;
+  private position_: Point;
+  private direction_: string;
 
   constructor() {
-    this.currentPosition = 0;
-    this.currentDirection = 1;
-    this.position = new Point(0, 0);
-    this.direction = "right";
+    //this.currentPosition = 0;
+    //this.currentDirection = 1;
+    this.position_ = new Point(0, 0);
+    this.direction_ = "right";
   }
   move(squares: number) {
-    if (this.direction === "up")
+    if (this.direction_ === "up")
       this.position = new Point(this.position.x, this.position.y - 1);
-    else if (this.direction === "down")
+    else if (this.direction_ === "down")
       this.position = new Point(this.position.x, this.position.y + 1);
-    else if (this.direction === "left")
+    else if (this.direction_ === "left")
       this.position = new Point(this.position.x - 1, this.position.y);
-    else if (this.direction === "right")
+    else if (this.direction_ === "right")
       this.position = new Point(this.position.x + 1, this.position.y);
   }
 
   turnLeft() {
-    if (this.direction === "up") this.direction = "left";
-    else if (this.direction === "left") this.direction = "down";
-    else if (this.direction === "down") this.direction = "right";
-    else if (this.direction === "right") this.direction = "up";
+    if (this.direction_ === "up") this.direction_ = "left";
+    else if (this.direction_ === "left") this.direction_ = "down";
+    else if (this.direction_ === "down") this.direction_ = "right";
+    else if (this.direction_ === "right") this.direction_ = "up";
   }
 
   turnRight() {
-    if (this.direction === "up") this.direction = "right";
-    else if (this.direction === "right") this.direction = "down";
-    else if (this.direction === "down") this.direction = "left";
-    else if (this.direction === "left") this.direction = "up";
+    if (this.direction_ === "up") this.direction_ = "right";
+    else if (this.direction_ === "right") this.direction_ = "down";
+    else if (this.direction_ === "down") this.direction_ = "left";
+    else if (this.direction_ === "left") this.direction_ = "up";
   }
 
   /*
@@ -51,12 +51,12 @@ class Snake {
   }
   */
 
-  public get positions() {
-    return this.currentPosition;
+  public get position() {
+    return this.position_;
   }
 
-  public get directions() {
-    return this.currentDirection;
+  public get direction() {
+    return this.direction_;
   }
 }
 
