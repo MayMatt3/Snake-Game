5bdea1ba9012d7444c0a9e9e0b194e4c67452da1
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-02-29 01:02:26 -0500

    Update AvoidWallsPlayer.ts

--- sandbox (5)/src/AvoidWallsPlayer.ts
+++ sandbox (5)/src/AvoidWallsPlayer.ts
@@ -6,17 +6,49 @@ class AvoidWallsPlayer extends Player {
     super(sc);
   }
 
-  makeTurn() {
+ makeTurn() {
     if (
       this.sc.snakePosition.x === 0 &&
       this.sc.snakeDirection === "left" &&
-      this.sc.snakePosition.y < this.sc.worldHeight
+      this.sc.snakePosition.y < this.sc.worldHeight / 2
     ) {
       this.sc.turnSnakeLeft();
     } else if (
-      this.sc.snakePosition.x === -1 &&
+      this.sc.snakePosition.x === 0 &&
+      this.sc.snakeDirection === "left"
+    ) {
+      this.sc.turnSnakeRight();
+    } else if (
+      this.sc.snakePosition.x === 50 &&
       this.sc.snakeDirection === "right" &&
-      this.sc.snakePosition.y > this.sc.worldHeight
+      this.sc.snakePosition.y < this.sc.worldHeight / 2
+    ) {
+      this.sc.turnSnakeRight();
+    } else if (
+      this.sc.snakePosition.x === 50 &&
+      this.sc.snakeDirection === "right"
+    ) {
+      this.sc.turnSnakeLeft();
+    } else if (
+      this.sc.snakePosition.y === 0 &&
+      this.sc.snakeDirection === "down" &&
+      this.sc.snakePosition.x < this.sc.worldWidth / 2
+    ) {
+      this.sc.turnSnakeLeft();
+    } else if (
+      this.sc.snakePosition.y === 0 &&
+      this.sc.snakeDirection === "down"
+    ) {
+      this.sc.turnSnakeRight();
+    } else if (
+      this.sc.snakePosition.y === 50 &&
+      this.sc.snakeDirection === "up" &&
+      this.sc.snakePosition.y < this.sc.worldWidth / 2
+    ) {
+      this.sc.turnSnakeLeft();
+    } else if (
+      this.sc.snakePosition.y === 50 &&
+      this.sc.snakeDirection === "up"
     ) {
       this.sc.turnSnakeRight();
     }
