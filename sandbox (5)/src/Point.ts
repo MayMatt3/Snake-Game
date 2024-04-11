be37949308fada2438c4c8e1ee22fb47ec65ac15
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-04-11 03:23:20 -0400

    Update Point.ts

--- sandbox (5)/src/Point.ts
+++ sandbox (5)/src/Point.ts
@@ -1,17 +1,32 @@
 import display from "./display";
 
+/**
+ * Represents a point
+ */
 class Point {
   private xcoord: number;
   private ycoord: number;
+
+  /**
+   * Creates a new point with coordinates
+   * @param x - the x-coordinate of the point
+   * @param y - the y-coordinate of the point
+   */
   constructor(x: number, y: number) {
     this.xcoord = x;
     this.ycoord = y;
   }
 
+  /**
+   * Gets the x-coordinate of the point
+   */
   public get x() {
     return this.xcoord;
   }
 
+  /**
+   * Gets the y-coordinate of the point
+   */
   public get y() {
     return this.ycoord;
   }
