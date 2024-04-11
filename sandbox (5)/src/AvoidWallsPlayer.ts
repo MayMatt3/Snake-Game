a1494ba8c0a9d4de846715d151ea152b6f8d88fb
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-04-11 03:25:16 -0400

    Update AvoidWallsPlayer.ts

--- sandbox (5)/src/AvoidWallsPlayer.ts
+++ sandbox (5)/src/AvoidWallsPlayer.ts
@@ -1,12 +1,23 @@
 import Player from "./Player";
 import SnakeController from "./SnakeController";
 
+/**
+ * Represents a player that tries to avoid walls in the game
+ * Inherits from the player class
+ */
 class AvoidWallsPlayer extends Player {
+  /**
+   * Creates a new AvoidWallsPlayer
+   */
   constructor(sc: SnakeController) {
     super(sc);
   }
 
- makeTurn() {
+  /**
+   * Makes a turn based on the current position of the snake and its direction, attempting to avoid walls
+   * Turns the snake left or right depending on its position relative to the walls
+   */
+  makeTurn() {
     if (
       this.sc.snakePosition.x === 0 &&
       this.sc.snakeDirection === "left" &&
@@ -19,13 +30,13 @@ class AvoidWallsPlayer extends Player {
     ) {
       this.sc.turnSnakeRight();
     } else if (
-      this.sc.snakePosition.x === 25 &&
+      this.sc.snakePosition.x === 50 &&
       this.sc.snakeDirection === "right" &&
       this.sc.snakePosition.y < this.sc.worldHeight / 2
     ) {
       this.sc.turnSnakeRight();
     } else if (
-      this.sc.snakePosition.x === 25 &&
+      this.sc.snakePosition.x === 50 &&
       this.sc.snakeDirection === "right"
     ) {
       this.sc.turnSnakeLeft();
@@ -41,17 +52,18 @@ class AvoidWallsPlayer extends Player {
     ) {
       this.sc.turnSnakeRight();
     } else if (
-      this.sc.snakePosition.y === 25 &&
+      this.sc.snakePosition.y === 50 &&
       this.sc.snakeDirection === "up" &&
       this.sc.snakePosition.y < this.sc.worldWidth / 2
     ) {
       this.sc.turnSnakeLeft();
     } else if (
-      this.sc.snakePosition.y === 25 &&
+      this.sc.snakePosition.y === 50 &&
       this.sc.snakeDirection === "up"
     ) {
       this.sc.turnSnakeRight();
     }
   }
 }
+
 export default AvoidWallsPlayer;
