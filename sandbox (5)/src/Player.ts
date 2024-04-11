dbac518fa21ad8b4d537f57102dfe6913ad5c732
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-04-11 03:22:57 -0400

    Update Player.ts

--- sandbox (5)/src/Player.ts
+++ sandbox (5)/src/Player.ts
@@ -1,9 +1,16 @@
 import display from "./display";
 import SnakeController from "./SnakeController";
 
+/**
+ * Represents a player in the game
+ */
 abstract class Player {
   protected sc: SnakeController;
 
+  /**
+   * Creates a new player
+   * @param snakecontroller - the snake controller represents the player's interaction with the game
+   */
   constructor(sc: SnakeController) {
     this.sc = sc;
   }
