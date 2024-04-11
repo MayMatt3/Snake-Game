57355dafd675015d70e32324d412ebcfb7a079d8
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-04-11 03:19:45 -0400

    Update Snake.ts

--- sandbox (5)/src/Snake.ts
+++ sandbox (5)/src/Snake.ts
@@ -4,29 +4,41 @@ import Point from "./Point";
 
 // place your code on line 5 above the export statement below
 
+/**
+ * Class represents a snake
+ */
 class Snake {
   //private currentPosition: number;
   //private currentDirection: number;
   private position_: Point;
   private direction_: string;
-
+  /**
+   * Create a snake
+   */
   constructor() {
     //this.currentPosition = 0;
     //this.currentDirection = 1;
     this.position_ = new Point(0, 0);
     this.direction_ = "right";
   }
+
+  /**
+   * moves the snake by a number of squares based on its current direction
+   */
   move(squares: number) {
     if (this.direction_ === "up")
-      this.position = new Point(this.position.x, this.position.y - 1);
+      this.position_ = new Point(this.position.x, this.position.y - 1);
     else if (this.direction_ === "down")
-      this.position = new Point(this.position.x, this.position.y + 1);
+      this.position_ = new Point(this.position.x, this.position.y + 1);
     else if (this.direction_ === "left")
-      this.position = new Point(this.position.x - 1, this.position.y);
+      this.position_ = new Point(this.position.x - 1, this.position.y);
     else if (this.direction_ === "right")
-      this.position = new Point(this.position.x + 1, this.position.y);
+      this.position_ = new Point(this.position.x + 1, this.position.y);
   }
 
+  /**
+   * turns the snake left
+   */
   turnLeft() {
     if (this.direction_ === "up") this.direction_ = "left";
     else if (this.direction_ === "left") this.direction_ = "down";
@@ -34,6 +46,9 @@ class Snake {
     else if (this.direction_ === "right") this.direction_ = "up";
   }
 
+  /**
+   * turns the snake right
+   */
   turnRight() {
     if (this.direction_ === "up") this.direction_ = "right";
     else if (this.direction_ === "right") this.direction_ = "down";
@@ -41,20 +56,24 @@ class Snake {
     else if (this.direction_ === "left") this.direction_ = "up";
   }
 
-  /*
-  turn() {
+  /**turn() {
     if (this.currentDirection === 1) {
       this.currentDirection = -1;
     } else if (this.currentDirection === -1) {
       this.currentDirection = 1;
     }
-  }
-  */
+  }*/
 
+  /**
+   * gets the current position of the snake
+   */
   public get position() {
     return this.position_;
   }
 
+  /**
+   * gets the current direction of the snake
+   */
   public get direction() {
     return this.direction_;
   }
