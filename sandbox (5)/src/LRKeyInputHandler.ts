2caa01da8accc4e690b8139e3804fbfadc7a8d22
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-04-11 03:26:52 -0400

    Update LRKeyInputHandler.ts

--- sandbox (5)/src/LRKeyInputHandler.ts
+++ sandbox (5)/src/LRKeyInputHandler.ts
@@ -1,9 +1,17 @@
 import IInputHandler from "./IInputHandler";
 
+/**
+ * Represents an input handler for left and right arrow keys
+ * Implements the IInputHandler interface
+ */
 class LRKeyInputHandler implements IInputHandler {
   private wasLeftArrowPushed: boolean;
   private wasRightArrowPushed: boolean;
 
+  /**
+   * Creates a LRKeyInputHandler
+   * Initializes the state of left and right arrow key presses
+   */
   constructor() {
     this.wasLeftArrowPushed = false;
     this.wasRightArrowPushed = false;
@@ -16,19 +24,31 @@ class LRKeyInputHandler implements IInputHandler {
     });
   }
 
-  madeLeftMove(): boolean {
+  /**
+   * Checks if a left arrow key was pressed
+   * @returns True if a key was pressed, false otherwise
+   */
+  madeLeftMove() {
     return this.wasLeftArrowPushed;
   }
 
-  madeRightMove(): boolean {
+  /**
+   * Checks if a right arrow key was pressed
+   */
+  madeRightMove() {
     return this.wasRightArrowPushed;
   }
 
-  resetLeftMove(): void {
+  /**
+   * Resets the state of left or right arrow key detection
+   */
+  resetLeftMove() {
     this.wasLeftArrowPushed = false;
   }
 
-  resetRightMove(): void {
+  resetRightMove() {
     this.wasRightArrowPushed = false;
   }
 }
+
+export default LRKeyInputHandler;
