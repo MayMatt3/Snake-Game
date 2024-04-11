fc0476533876bda4b48ad2bab12e7a7bafc4d791
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-04-11 03:29:02 -0400

    Update GameController.ts

--- sandbox (5)/src/GameController.ts
+++ sandbox (5)/src/GameController.ts
@@ -1,27 +1,44 @@
 import WorldModel from "./WorldModel";
 import Player from "./Player";
 
+/**
+ * Represents the controller for the game
+ */
 class GameController {
   private world: WorldModel;
   private player1_: Player | null;
   private player2_: Player | null;
 
   constructor(world: WorldModel) {
+    /**
+     * Creates a GameController
+     */
     this.world = world;
     this.player1_ = null;
     this.player2_ = null;
   }
 
+  /**
+   * Sets the first player of the game
+   * @param player - The player representing the first player
+   */
   set player1(player: Player) {
     this.player1_ = player;
   }
+
+  /**
+   * Sets the second player of the game
+   * @param player - The player object representing the second player
+   */
   set player2(player: Player) {
     this.player2_ = player;
   }
 
   private lastTime: number = 0;
 
-  // runs the game
+  /**
+   * Runs the game loop, updating game state and player actions
+   */
   run(): void {
     let lastTime = 0;
     let updateFrame = (milliseconds: number) => {
