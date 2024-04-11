1891b34f75365651147c0b76c423d46fa8572cac
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-04-11 03:26:15 -0400

    Update IInputHandler.ts

--- sandbox (5)/src/IInputHandler.ts
+++ sandbox (5)/src/IInputHandler.ts
@@ -1,4 +1,13 @@
+/**
+ * Represents an interface for handling input in the game
+ * Provides methods for detecting left and right moves, and resetting them
+ */
 interface IInputHandler {
+  /**
+   * Checks if a left or right move was made
+   * @returns true if a left moves was made, false otherwise
+   * resets the state of left and right move dectection
+   */
   madeLeftMove(): boolean;
   madeRightMove(): boolean;
   resetLeftMove(): void;
