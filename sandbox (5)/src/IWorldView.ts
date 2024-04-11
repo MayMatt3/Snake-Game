0c7cb30174bdac020d630a74a0b8895a3541884d
MayMatt3 <156825506+MayMatt3@users.noreply.github.com>
2024-04-11 03:23:56 -0400

    Update IWorldView.ts

--- sandbox (5)/src/IWorldView.ts
+++ sandbox (5)/src/IWorldView.ts
@@ -1,7 +1,13 @@
 import WorldModel from "./WorldModel";
-
+/**
+ * Represents a view interface for displaying the game world
+ * Defines a mehtod for displaying the game world based on a provided world model
+ */
 interface IWorldView {
-  displayWorld(worldModel: WorldModel): void;
+  /**
+   * Displays the game world based on the provided world model
+   */
+  display(worldModel: WorldModel): void;
 }
 
 export default IWorldView;
